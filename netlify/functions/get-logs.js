de37188ae3624371a6a771d71b71055f8666359a
huseynw <huseynmemmedov9272@gmail.com>
2026-04-07 19:49:47 +0400

    Update get-logs.js

--- netlify/functions/get-logs.js
+++ netlify/functions/get-logs.js
@@ -1,20 +1,24 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
+    // Brauzerdən gələn məlumatları alırıq
     const { url, apiKey } = JSON.parse(event.body);
 
     try {
+        // Node-un daxili fetch funksiyasından istifadə edirik (Heç bir require lazım deyil)
         const response = await fetch(url, {
             headers: {
                 "Accept": "application/vnd.heroku+json; version=3",
                 "Authorization": `Bearer ${apiKey}`
             }
         });
+        
         const data = await response.text();
 
         return {
             statusCode: 200,
-            headers: { "Access-Control-Allow-Origin": "*" },
+            headers: { 
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "text/plain" 
+            },
             body: data
         };
     } catch (error) {
